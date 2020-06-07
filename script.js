9656e9303de3d29299dd94a51045b5bcec5b009a
damilola <dammyadeniye@gmail.com>
2020-06-07 12:46:01 +0100

    done lodash and package,json

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-import { without } from "lodash";
+//import { without } from "lodash";
+
+var _ = require("lodash");
+var array = [1, 2, 3, 4, 5];
+console.log("answer:", _.without(array, 3));
+
+//alert("hello WORLD");
